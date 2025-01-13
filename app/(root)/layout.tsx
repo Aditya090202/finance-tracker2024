b9bb7461faf1703992d4abec77516db3ca224584
Aditya090202 <amathur7@buffalo.edu>
2025-01-13 00:32:54 -0500

    Add new components for banking features and update layout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import Sidebar from "@/components/Sidebar";
-
+import Image from "next/image";
+import MobileNavBar from "@/components/MobileNavBar";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,7 +10,15 @@ export default function RootLayout({
   return (
     <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn}></Sidebar>
-      {children}
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="menu icon" />
+          <div>
+            <MobileNavBar user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,33 @@
 import React from "react";
 import HeaderBox from "@/components/HeaderBox";
 import BalanceBox from "@/components/BalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 const Home = () => {
   const loggedIn = {
     firstName: "Aditya",
+    lastName: "Mathur",
+    email: "adityam.jobs@gmail.com",
   };
   return (
     <section className="home">
       <div className="home-content">
-        <HeaderBox
-          type="greeting"
-          title="Welcome"
-          user={loggedIn?.firstName || "Guest"}
-          subtext="Access and manage your account and transactions efficiently"
-        />
-        <BalanceBox accounts={[]} numOfBanks={1} currentBalance={2257.49} />
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <BalanceBox accounts={[]} numOfBanks={3} currentBalance={2257.49} />
+        </header>
+        recent transactions
       </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 2267.49 }]}
+      />
     </section>
   );
 };
